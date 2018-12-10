2ca91e949961b0e6e6010b88b3c620d215fc9108
horosora <horosora@gmail.com>
2018-12-10 16:27:26 +0900

    update

--- pwn_template/template2_solve.py
+++ pwn_template/template2_solve.py
@@ -8,14 +8,12 @@ from pwn import *
 context(os="linux", arch="i386")
 #context.log_level = "debug"
 
-if len(sys.argv) > 1 and sys.argv[2] == "r":
+if len(sys.argv) == 2 and sys.argv[1] == "r":
     conn = remote("0.1.2.3", 1000)
 else:
     conn = process("./hoge")
 
 #conn.recvuntil("hoge")
-
-payload = ""
-
+#payload = "hoge"
 #conn.sendline(payload)
 #conn.interactive()
